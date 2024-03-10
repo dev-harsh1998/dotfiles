6bf5ed7f2992f6d89a8ff99f265f0fcaaff62a8b
Harshit Jain <reach@harsh1998.dev>
2024-03-10 09:13:02 +0530

    code.smol

--- RecordingSorter.java
+++ RecordingSorter.java
@@ -9,7 +9,6 @@ import java.util.regex.Pattern;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Set;
 import java.util.regex.Matcher;
 
@@ -59,39 +58,7 @@ final class CallFrequency {
 }
 
 public class RecordingSorter {
-
-    private static String getMonthString(String month) {
-        switch (month) {
-            case "01":
-                return "January";
-            case "02":
-                return "February";
-            case "03":
-                return "March";
-            case "04":
-                return "April";
-            case "05":
-                return "May";
-            case "06":
-                return "June";
-            case "07":
-                return "July";
-            case "08":
-                return "August";
-            case "09":
-                return "September";
-            case "10":
-                return "October";
-            case "11":
-                return "November";
-            case "12":
-                return "December";
-            default:
-                return "Invalid month";
-        }
-    }
-
-    private static void createData(String folderPath, String outputFolder) {
+    private static void createData(String folderPath, String folderPathOut) {
         HashMap<String, CallFrequency> recordingFileNamesFreqency = new HashMap<String, CallFrequency>();
         File folder = new File(folderPath);
         File[] listOfFiles = folder.listFiles();
@@ -126,67 +93,45 @@ public class RecordingSorter {
         }
 
         // Call create_data function with the recordingFileNamesFreqency.
-        createDataStore(recordingFileNamesFreqency, outputFolder);
-    }
-
-    private static void createDirectoryStructure(Queue<String> directoryOrder) {
-        while (!directoryOrder.isEmpty()) {
-            String directoryPath = directoryOrder.poll();
-            File directory = new File(directoryPath);
-            if (!directory.exists()) {
-                directory.mkdirs();
-            }
-        }
-
+        createDataStore(recordingFileNamesFreqency, folderPathOut);
     }
 
     private static void createDataStore(HashMap<String, CallFrequency> recordingFileNamesFreqency,
-            String outputFolder) {
-        Queue<String> directoryOrder = new LinkedList<>();
-        File folder = new File(outputFolder);
+            String folderPathOut) {
+        File folder = new File("/home/harsh/Documents/code/data/output");
+        folder.mkdirs();
         for (String key : recordingFileNamesFreqency.keySet()) {
-            directoryOrder.add(folder.getAbsolutePath());
-            File nameFolder = new File(outputFolder + "/" + recordingFileNamesFreqency.get(key).name);
-            directoryOrder.add(nameFolder.getAbsolutePath());
-            for (int i = 0; i < recordingFileNamesFreqency.get(key).recordingMetadata.RecordingDate.size(); i++) {
-                String date = recordingFileNamesFreqency.get(key).recordingMetadata.RecordingDate.get(i);
-                String time = recordingFileNamesFreqency.get(key).recordingMetadata.RecordingTime.get(i);
-                // Date: yy -> yyyy
-                // Date: mm -> January - December
-                // Date: dd -> 01 - 31
-                // Time: hh -> 12:00 AM - 11:59 PM
-                String year = "20" + date.substring(0, 2);
-                String month = getMonthString(date.substring(2, 4));
-                String day = date.substring(4, 6);
-                String hour = time.substring(0, 2);
-                // convert hour to 12 hour format. just Substract 12 if it is greater than 12
-                // and append PM else append AM
-                if (Integer.parseInt(hour) > 12) {
-                    hour = Integer.toString(Integer.parseInt(hour) - 12) + "-PM";
-                } else {
-                    hour = hour + "-AM";
-                }
-                String directoryPath = outputFolder + "/" + recordingFileNamesFreqency.get(key).name + "/" + year + "/"
-                        + month + "/" + day + "/" + hour;
-                File directory = new File(directoryPath);
-                directoryOrder.add(directory.getAbsolutePath());
-                createDirectoryStructure(directoryOrder);
-                for (String recordingFile : recordingFileNamesFreqency.get(key).recordingMetadata.RecordingFile) {
-                    File file = new File(recordingFile);
-                    file.renameTo(new File(directoryPath + "/" + file.getName()));
-                }
-                directoryOrder.clear();
+            String name = recordingFileNamesFreqency.get(key).name;
+            for (int i = 0; i < recordingFileNamesFreqency.get(key).recordingMetadata.RecordingFile.size(); i++) {
+                String recordingFile = recordingFileNamesFreqency.get(key).recordingMetadata.RecordingFile.toArray()[i]
+                        .toString();
+                String recordingDate = recordingFileNamesFreqency.get(key).recordingMetadata.RecordingDate.toArray()[i]
+                        .toString();
+                String recordingTime = recordingFileNamesFreqency.get(key).recordingMetadata.RecordingTime.toArray()[i]
+                        .toString();
+                String year = recordingDate.substring(0, 2);
+                String month = recordingDate.substring(2, 4);
+                String date = recordingDate.substring(4, 6);
+                String hour = recordingTime.substring(0, 2);
+                String newFolderPath = "/home/harsh/Documents/code/data/output/" + name + "/" + year + "/" + month + "/"
+                        + date + "/" + hour;
+                File newFolder = new File(newFolderPath);
+                newFolder.mkdirs();
+                File file = new File(recordingFile);
+                file.renameTo(new File(newFolderPath + "/" + file.getName()));
             }
         }
     }
 
     public static void main(String[] args) {
+        // 2 arguments are expected. The first argument is the input folder path and the
+        // second argument is the output folder path.
         if (args.length != 2) {
-            System.out.println("Usage: java RecordingSorter <input_folder> <output_folder>");
+            System.out.println("Invalid number of arguments. Please provide the input and output folder paths.");
             return;
         }
-        String inputFolder = args[0];
-        String outputFolder = args[1];
-        createData(inputFolder, outputFolder);
+        String folderPathIn = args[0];
+        String folderPathOut = args[1];
+        createData(folderPathIn, folderPathOut);
     }
 }
